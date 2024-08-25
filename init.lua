320667066f9a889a6fd90652c4ab435da9e43455
Jakub Woźniak <jakub.wozniak@nordcloud.com>
2024-08-25 22:53:51 +0200

    chore: change theme to github-dark-dimmed

--- init.lua
+++ init.lua
@@ -591,10 +591,10 @@ require('lazy').setup({
   },
 
   { -- You can easily change to a different colorscheme.
-    'folke/tokyonight.nvim',
+    'projekt0n/github-nvim-theme',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'github_dark_dimmed'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
