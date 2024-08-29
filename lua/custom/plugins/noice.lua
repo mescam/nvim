97f91f82881eb012af45624a038d60da4eb0dc34
Jakub Woźniak <jakub.wozniak@nordcloud.com>
2024-08-29 09:55:13 +0200

    chore: noice.vim config change

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -2,6 +2,19 @@ return {
   {
     'folke/noice.nvim',
     event = 'VeryLazy',
+    lsp = {
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true,
+      },
+    },
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      lsp_doc_border = false,
+    },
     opts = {
       -- add any options here
     },
